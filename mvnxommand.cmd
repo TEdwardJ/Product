db16889b18b0f09ca9c6d2981511532c2ca4238c
JavaTed <30757832+JavaTed@users.noreply.github.com>
2018-10-27 16:53:55 +0300

    added procfile

---
+++ mvnxommand.cmd
@@ -0,0 +1,1 @@
+C:\JavaCourse\Product>"C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2018.1.5\plugins\maven\lib\maven3\bin\mvn" deploy:deploy-file -Durl=file://./repo/ -Dfile=ioc-container-1.0-SNAPSHOT.jar -DgroupId=edu.eteslenko -DartifactId=ioc-container -Dpackaging=jar -Dversion=1.0-SNAPSHOT
